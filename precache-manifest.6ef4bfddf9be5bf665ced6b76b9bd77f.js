871725b85aa054f74efa13218813800ce3fd1927
Joshua Soller <joshua.soller@outlook.com>
2020-02-24 21:40:03 +0800

    Updates

---
+++ precache-manifest.6ef4bfddf9be5bf665ced6b76b9bd77f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "304430c09f09baf9bfc88c717eb68595",
+    "url": "/testing_upload/index.html"
+  },
+  {
+    "revision": "8c872cbed02be8e5eec9",
+    "url": "/testing_upload/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4c4c283c70a220943795",
+    "url": "/testing_upload/static/js/2.89fd05fd.chunk.js"
+  },
+  {
+    "revision": "a760da7e0d18b9757b85d79046d4cbf9",
+    "url": "/testing_upload/static/js/2.89fd05fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c872cbed02be8e5eec9",
+    "url": "/testing_upload/static/js/main.0a028ad4.chunk.js"
+  },
+  {
+    "revision": "1db358d8005680fcd3fb",
+    "url": "/testing_upload/static/js/runtime-main.4a89d132.js"
+  }
+]);
